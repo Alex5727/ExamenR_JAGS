00bbd7ee48f596aba11e2e87626002558adbeee5
Alex5727 <alejandrogrjalva@gmail.com>
2024-12-10 11:28:45 -0700

    Agregar archivos de proyecto.

---
+++ WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ExamenR_JAGS.WebForm1"  Async="true"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="align-items:center">
+            <asp:Label Text="DOG API"  runat="server"/>
+            <br />
+            <asp:Button Text="Generar" OnClick="Generar" runat="server" />
+             <br />
+            <asp:Image  ImageUrl="" runat="server" ID="imagen" Visible="false" Width="400px" Height="400px"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExamenR_JAGS
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public class Perrito
+        {
+            public string status { get; set; }
+            public string message { get; set; }
+        }
+
+        protected async void Generar(object sender, EventArgs e)
+        {
+            await GenerarPerrito();
+        }
+
+
+        public async  Task GenerarPerrito()
+        {
+            string url = "https://dog.ceo/api/breeds/image/random";
+            using (HttpClient  client = new HttpClient())
+            {
+                var json = await client.GetStringAsync(url);
+                Perrito perrito = JsonConvert.DeserializeObject<Perrito>(json);
+
+                imagen.ImageUrl = perrito.message;
+                imagen.Visible = true;
+            }
+        }
+
+
+    }
+}
